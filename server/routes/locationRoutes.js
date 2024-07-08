91ca622be59bb427de7b19680b11538295fe9210
orimaromcom <ori.marom10@gmail.com>
2024-07-08 11:17:35 +0300

    add get location

--- server/routes/locationRoutes.js
+++ server/routes/locationRoutes.js
@@ -3,9 +3,24 @@ import express from "express";
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
-  res.status(200).json({
-    message: "Hello from ISS",
-  });
+  try {
+    const location = true;
+
+    const response = await fetch("http://api.open-notify.org/iss-now.json", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("getting ISS location");
+    res.status(200).json({ success: true, data: await response.json() });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Fetching ISS location failed, please try again",
+    });
+  }
 });
 
 export default router;
